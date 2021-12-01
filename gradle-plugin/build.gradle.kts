ad67716505cf845ac375753751e672ba9ed4be3d
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-12-01 13:56:56 +0100

    Remove unnecessary `kotlin.explicitApi()`

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -26,10 +26,6 @@ gradlePlugin {
     }
 }
 
-kotlin {
-    explicitApi()
-}
-
 dependencies {
     // TODO: remove when the next issue is fixed:
     //  https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
